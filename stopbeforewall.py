ea215825054a9071ba5c243a0a2bb8a7051fb6bb
Luis Mojica <lmojica19@bsmschool.org>
2018-05-01 10:15:16 -0500

    changed motors

--- stopbeforewall.py
+++ stopbeforewall.py
@@ -3,10 +3,10 @@ import RoboPiLib as RPL
 RPL.RoboPiInit("/dev/ttyAMA0",115200)
 
 # Move forward
-RPL.servoWrite(0,1000)
-RPL.servoWrite(1,2000)
+RPL.servoWrite(1,1000)
+RPL.servoWrite(2,2000)
 
 # Stop moving
 if RPL.digitalRead(16) == 0:
-    RPL.servoWrite(0,0)
     RPL.servoWrite(1,0)
+    RPL.servoWrite(2,0)
