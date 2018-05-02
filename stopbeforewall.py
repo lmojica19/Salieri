1518058bc883becac30ff3135d2abb57a0101494
Luis Mojica <lmojica19@bsmschool.org>
2018-05-02 10:26:34 -0500

    changed if to while

--- stopbeforewall.py
+++ stopbeforewall.py
@@ -3,14 +3,19 @@ import RoboPiLib as RPL
 RPL.RoboPiInit("/dev/ttyAMA0",115200)
 
 # Move forward
-RPL.servoWrite(1,1000)
-RPL.servoWrite(2,2000)
+while RPL.digitalRead(17) == 1:
+    RPL.servoWrite(1,1000)
+    RPL.servoWrite(2,2000)
+else:
+    break
 
 # Stop moving
-if RPL.digitalRead(17) == 0:
+while RPL.digitalRead(17) == 0:
     RPL.servoWrite(1,0)
     RPL.servoWrite(2,0)
-    
+else:
+    break
+
 """
 
 ssh student@192.168.21.121
