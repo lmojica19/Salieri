d1e6b6a88a2b5a8bfc711e2ca31e942d48b2c2da
Luis Mojica <lmojica19@bsmschool.org>
2018-05-09 10:01:59 -0500

    initial clutcher test

---
+++ clutcher.py
@@ -0,0 +1,15 @@
+import threading
+import RoboPiLib as RPL
+RPL.RoboPiInit("/dev/ttyAMA0",115200)
+
+def runner():
+    while True:
+        RPL.servoWrite(1,1000)
+        RPL.servoWrite(2,2000)
+        threading.Timer(5.0, printit).start()
+        RPL.servoWrite(0,0)
+        RPL.servoWrite(1,0)
+        RPL.servoWrite(2,0)
+        runner()
+
+runner()
