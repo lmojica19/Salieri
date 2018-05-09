b625a6c5955f533000a6370ee71de9b0d3fc474b
Luis Mojica <lmojica19@bsmschool.org>
2018-05-09 10:09:52 -0500

    fixed variables

--- clutcher.py
+++ clutcher.py
@@ -2,14 +2,15 @@ import threading
 import RoboPiLib as RPL
 RPL.RoboPiInit("/dev/ttyAMA0",115200)
 
-while True:
-    RPL.servoWrite(1,1000)
-    RPL.servoWrite(2,2000)
-    threading.Timer(3.0, printit).start()
-    RPL.servoWrite(0,0)
-    RPL.servoWrite(1,0)
-    RPL.servoWrite(2,0)
-    threading.Timer(3.0, printit).start()
+def runner():
+    while True:
+        RPL.servoWrite(1,1000)
+        RPL.servoWrite(2,2000)
+        threading.Timer(3.0, runner).start()
+        RPL.servoWrite(0,0)
+        RPL.servoWrite(1,0)
+        RPL.servoWrite(2,0)
+        threading.Timer(3.0, runner).start()
 
 """
 
