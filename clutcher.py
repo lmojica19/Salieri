a72bb885ddf730f95bfe1510f73c704218caabf9
Luis Mojica <lmojica19@bsmschool.org>
2018-05-09 10:06:43 -0500

    changed seconds from 5 to 3

--- clutcher.py
+++ clutcher.py
@@ -6,11 +6,11 @@ def runner():
     while True:
         RPL.servoWrite(1,1000)
         RPL.servoWrite(2,2000)
-        threading.Timer(5.0, printit).start()
+        threading.Timer(3.0, printit).start()
         RPL.servoWrite(0,0)
         RPL.servoWrite(1,0)
         RPL.servoWrite(2,0)
-        threading.Timer(5.0, printit).start()
+        threading.Timer(3.0, printit).start()
         runner()
 
 runner()
