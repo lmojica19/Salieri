edd38c2dc8085fa9fb0986a5f6490820482ed413
Luis Mojica <lmojica19@bsmschool.org>
2018-05-09 10:42:50 -0500

    hopefully this new stuff works

--- clutcher.py
+++ clutcher.py
@@ -2,22 +2,15 @@ import threading
 import RoboPiLib as RPL
 RPL.RoboPiInit("/dev/ttyAMA0",115200)
 
-def runner():
-    while True:
-        RPL.servoWrite(1,1000)
-        RPL.servoWrite(2,2000)
-        threading.Timer(3.0, runner).start()
-        RPL.servoWrite(0,0)
-        RPL.servoWrite(1,0)
-        RPL.servoWrite(2,0)
-        threading.Timer(3.0, runner).start()
+def susume():
+    RPL.servoWrite(1,1000)
+    RPL.servoWrite(2,2000)
+    threading.Timer(3, matte).start()
 
-runner()
+def matte():
+    RPL.servoWrite(0,0)
+    RPL.servoWrite(1,0)
+    RPL.servoWrite(2,0)
+    threading.Timer(3, susume).start()
 
-"""
-
-ssh student@192.168.21.121
-
-ctrl c to cease
-
-"""
+susume()
